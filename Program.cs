9afc9e791544278beb282b9a8eaacbc9071605eb
Innocent Angwadia <demsdema217@gmail.com>
2019-01-27 13:43:10 +0300

    Declare and assign variables

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Variables
 {
@@ -10,6 +6,32 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            //staticly typed language 
+
+            Random randomNumber = new Random();
+
+            int firstNumber = randomNumber.Next(2, 10);
+            int secondNumber = randomNumber.Next(2,10);
+
+            var substraction = randomNumber.Next(2,10);
+
+            int answer;
+
+            string promp = ". Don't type in the answer, just Press ENTER when ready";
+
+            Console.WriteLine("Think of a number between 1 and 10" + promp);
+            Console.ReadKey();
+            Console.WriteLine("Multiply your number by " + firstNumber + promp);
+            Console.ReadKey();
+            Console.WriteLine("Now multiply the result by " + secondNumber + promp);
+            Console.ReadKey();
+            Console.WriteLine("Divide the result by the number you originally thought of" + promp);
+            Console.ReadKey();
+            Console.WriteLine("Now substract 7" + promp);
+            Console.ReadKey();
+
+            answer = firstNumber * secondNumber -7;
+            Console.WriteLine("The answer is " + answer);
         }
     }
 }
